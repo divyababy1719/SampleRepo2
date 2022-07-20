215a8cba10f388104234e155329233685dd86e96
JOLLU KISHORE <JOLLU KISHORE@DESKTOP-IH4C39S>
2022-07-20 15:32:51 +0530

    final commit

--- src/test/java/PracticeTest.java
+++ src/test/java/PracticeTest.java
@@ -1,9 +1,16 @@
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class PracticeTest {
 @Test
 public void createProduct()
 {
+	WebDriver driver=null;
+	
+	String browser = System.getProperty("Browser");
+	String url = System.getProperty("url");
+	driver.get(url);
+	
 	System.out.println("product created");
 }
 }
